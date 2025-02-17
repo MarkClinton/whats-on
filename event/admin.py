aca9646ed096c9cd0faf6855a9a52c622e56b565
Mark <markc9309@yahoo.com>
2025-02-17 22:18:54 +0000

    feat: add event models to admin panel

--- event/admin.py
+++ event/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Event, Comments, Category
 
 # Register your models here.
+admin.site.register(Event)
+admin.site.register(Comments)
+admin.site.register(Category)
