3de63a77417a0bcc4e2976ba14faa7405a66bcc9
Mark <markc9309@yahoo.com>
2025-02-18 21:56:17 +0000

    feat: added styling and basic base.html

--- event/views.py
+++ event/views.py
@@ -3,6 +3,5 @@ from django.views import generic
 from .models import Event
 
 class EventList(generic.ListView):
-    queryset = Event.objects.all()
+    queryset = Event.objects.all()  # pylint: disable=no-member
     template_name = "event/index.html"
-    
