3dba8e985af15f1f94468d82d25f6931a764524d
Mark <markc9309@yahoo.com>
2025-02-20 15:36:45 +0000

    feat: added templates for event and hooked up urls

--- event/urls.py
+++ event/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.EventList.as_view(), name='home'),
+    path('attending/', views.event_attending, name='event_attending'),
+    path('hosting/', views.event_hosting, name='event_hosting'),
+    path('create/', views.event_create, name='event_create'),
+    path('', views.EventList.as_view(), name='event_home'),
 ]
--- event/views.py
+++ event/views.py
@@ -4,4 +4,37 @@ from .models import Event
 
 class EventList(generic.ListView):
     queryset = Event.objects.all()  # pylint: disable=no-member
-    template_name = "event/index.html"
+    template_name = "event/search.html"
+
+def event_create(request):
+    queryset = Event.objects.all()  # pylint: disable=no-member
+
+    return render(
+        request,
+        "event/create.html",
+        {
+            "event": queryset,
+        },
+    )
+
+def event_hosting(request):
+    queryset = Event.objects.all()  # pylint: disable=no-member
+
+    return render(
+        request,
+        "event/hosting.html",
+        {
+            "event": queryset,
+        },
+    )
+
+def event_attending(request):
+    queryset = Event.objects.all()  # pylint: disable=no-member
+
+    return render(
+        request,
+        "event/attending.html",
+        {
+            "event": queryset,
+        },
+    )
