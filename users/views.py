5addd8668597aed11b57eb056eb29496a5e69124
Mark <markc9309@yahoo.com>
2025-02-20 16:41:20 +0000

    feat: added template for user profile and root home. Hooked up nav links and routing

--- users/views.py
+++ users/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import NewUser
 
-# Create your views here.
+def get_profile(request):
+    queryset = NewUser.objects.all()  # pylint: disable=no-member
+
+    return render(
+        request,
+        "users/profile.html",
+        {
+            "profile": queryset,
+        },
+    )
--- whatson/urls.py
+++ whatson/urls.py
@@ -16,9 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
 
 urlpatterns = [
-    path('', include("event.urls"), name="event_urls"),
+    path('event/', include("event.urls"), name="event_urls"),
+    path('profile/', include("users.urls"), name="users_urls"),
     path("accounts/", include("allauth.urls")),
     path('admin/', admin.site.urls),
+    path('', TemplateView.as_view(template_name='home.html'), name="home")
 ]
