f6ed070081c1d209167059565680979033dcd876
MarkClinton <marklwclinton@gmail.com>
2025-03-02 16:45:42 +0000

    refactor: updated invalid message for phone number field and placeholder

--- users/forms.py
+++ users/forms.py
@@ -6,13 +6,15 @@ from .models import NewUser
 
 class ProfileForm(forms.ModelForm):
 
+    TEL_MESSAGE ='Incorrect format. Use an Irish number. e.g. (087) 456 7890.'
+
     about = forms.CharField(widget=forms.Textarea(attrs={'rows' : '3'}))
     phone_number = PhoneNumberField(
-        widget=forms.TextInput(attrs={'placeholder': 'Format: (123) 4567 890'}),
+        widget=forms.TextInput(attrs={'placeholder': 'Irish Phone Numbers Only'}),
         required=False,
         region="IE"
     )
-    phone_number.error_messages['invalid'] = 'Incorrect Format. Use Format (123)-456-7890.'
+    phone_number.error_messages['invalid'] = TEL_MESSAGE
     profile_image = CloudinaryFileField()
 
     class Meta:
